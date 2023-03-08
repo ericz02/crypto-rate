35332b64579a28bb666ca14c014c3abed3bb3bf4
ericz02 <ezheng0720@gmail.com>
2023-03-07 20:35:10 -0500

    Updated NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { useState, useEffect} from 'react'
-import axios from 'axios';
-import NavBar from './components/navBar';
-import './App.css'
+import React from "react"
+import NavBar from "./components/NavBar"
 
+function App() {
 
-// watched some video
-// function App() {
+  return (
+    <div className='App'>
+        <NavBar />
+    </div>
+  )
+}
 
-//   // Bitcoin Price Index
-//   const [bpiData, setBpiData] = useState({})
-
-//   const fetchApiData = async () => {
-//     const {data:{bpi}} = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-//     // const bpiDataDerived = Object.keys(bpi).reduce((acc, currentCode) => {
-//     //   acc[currentCode]= bpi[currentCode].rate_float
-//     //   return acc
-//     // }, [])
-//     // setBpiData(bpiDataDerived)
-//     setBpiData(bpi)
-//   }
-
-//   useEffect(() => {
-//     fetchApiData()
-    
-//   }, [])
-
-  
-//   return (
-//     <div className='App'>
-//       <pre>{JSON.stringify(bpiData,0,1)} </pre>
-
-//     </div>
-//   )
-// }
-
-// export default App
+export default App
